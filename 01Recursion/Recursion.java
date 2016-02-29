36838b1897e6d674866508fe9a8f869498b47551
wenjiazhang <wzhang3@stuy.edu>
2016-02-29 12:50:09 -0500

    Working Recursion

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -9,6 +9,8 @@ public class Recursion{
     public double sqrt(double n){
 	if (n<0){
 	    throw new IllegalArgumentException();
+	}else if(n==0){
+	    return 0;
 	}else{
 	    return sqrtH(n,1); 
 	}
